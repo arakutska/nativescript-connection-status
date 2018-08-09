a0e18c731ea007e2a9933fa84c2ecc3b9763329f
pawlak <jakub.pawlak@icloud.com>
2018-08-09 12:18:57 +0200

    Async pipe in app component template, added BehaviorSubject for initial state and handling multiple subscriptions

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,26 +1,13 @@
-import {Component, OnDestroy, OnInit} from "@angular/core";
+import {Component} from "@angular/core";
 import {InternetConnectionService} from "~/shared/services/internet-connection.service";
-import {Page} from "tns-core-modules/ui/page";
 
 @Component({
     selector: "ns-app",
     templateUrl: "app.component.html"
 })
-export class AppComponent implements OnInit, OnDestroy {
-    connectionStatus: boolean = false;
-    connection$;
+export class AppComponent {
 
-    constructor(private _internetConnection: InternetConnectionService) { }
-
-    ngOnInit(): void {
-        this.connection$ = this._internetConnection.getConnectionStatus().subscribe(data => {
-            this.connectionStatus = data.valueOf();
-        });
-    }
-
-    ngOnDestroy(): void {
-        if (this.connection$)
-            this.connection$.unsubscribe();
+    constructor(public internetConnection: InternetConnectionService) {
     }
 
 }
--- app/shared/services/internet-connection.service.ts
+++ app/shared/services/internet-connection.service.ts
@@ -1,20 +1,25 @@
-import {Injectable} from '@angular/core';
-import {startMonitoring} from "tns-core-modules/connectivity";
-import {Observable} from "rxjs/internal/Observable";
+import { Injectable } from '@angular/core';
+import { startMonitoring, getConnectionType } from "tns-core-modules/connectivity";
+import { Observable } from "rxjs/internal/Observable";
+import { BehaviorSubject } from "rxjs/internal/BehaviorSubject";
 
 @Injectable()
 export class InternetConnectionService {
 
-    getConnectionStatus(): Observable<boolean> {
+    private _connectionStatusSubject$: BehaviorSubject<boolean>;
 
-        return Observable.create(observer => {
-            startMonitoring(connectionType => {
-                if (connectionType == 0) {
-                    observer.next(false);
-                }
-                else
-                    observer.next(true);
-            });
-        })
+    constructor() {
+        this._connectionStatusSubject$ = new BehaviorSubject<boolean>(getConnectionType() != 0);
+        startMonitoring(connectionType => {
+            if (connectionType == 0) {
+                this._connectionStatusSubject$.next(false);
+            }
+            else
+                this._connectionStatusSubject$.next(true);
+        });
+    }
+
+    get connectionStatus$(): Observable<boolean> {
+        return this._connectionStatusSubject$.asObservable();
     }
 }
